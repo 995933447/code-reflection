1991b4b260d1874f8ebf4d38286674401f42565d
995933447 <34621597+995933447@users.noreply.github.com>
2020-02-18 17:18:59 +0800

    Update SimpleReflectionCode.php

--- src/SimpleReflectionCode.php
+++ src/SimpleReflectionCode.php
@@ -5,28 +5,28 @@ class SimpleReflectionCode
 {
 	public static function handle($reflector, int $startLine = null, int $endLine = null) : string
 	{
-	    if (!$reflector instanceof \Reflector) {
-	        if (is_array($reflector)) {
-	            $reflector = new \ReflectionMethod($reflector[0], $reflector[1]);
-            } else {
-                $reflector = new \ReflectionClass($reflector);
-            }
-        }
+		if (!$reflector instanceof \Reflector) {
+	        	if (is_array($reflector)) {
+	            		$reflector = new \ReflectionMethod($reflector[0], $reflector[1]);
+            		} else {
+                		$reflector = new \ReflectionClass($reflector);
+            		}
+        	}
 
-	    $path = $reflector->getFileName();
+		$path = $reflector->getFileName();
 		$lines = file($path);
 
-        $from = $reflector->getStartLine();
+        	$from = $reflector->getStartLine();
 
 		if ($endLine) {
-            $end = $from + $endLine - 1;
-        } else {
-            $end = $reflector->getEndLine();
-        }
+            		$end = $from + $endLine - 1;
+        	} else {
+            		$end = $reflector->getEndLine();
+        	}
 
-        if ($startLine) {
-            $from = $from - 1 + ($startLine - 1);
-        }
+        	if ($startLine) {
+            		$from = $from - 1 + ($startLine - 1);
+        	}
 
 		return implode(array_slice($lines, $from - 1, $end - $from + 1));
 	}
